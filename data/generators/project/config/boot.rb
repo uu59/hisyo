a3812cdf3c1846517d39663c9a01ba53b814ed78
uu59 <k@uu59.org>
2012-05-28 02:50:24 +0900

    fixed boot.rb when RACK_ENV is empty

--- data/generators/project/config/boot.rb
+++ data/generators/project/config/boot.rb
@@ -1,7 +1,7 @@
 # Setup app 
 require "rubygems"
 require "bundler/setup"
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require(:default, ENV["RACK_ENV"] || "development")
 
 # load .rb files at lib/**, config/**, app/**
 Hisyo.setup
