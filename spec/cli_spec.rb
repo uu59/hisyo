9669264bcdfd3eb273a90ea3bee3178c70e162b9
uu59 <k@uu59.org>
2012-06-29 02:24:15 +0900

    correct describe and it text

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
-describe "Hisyo::CLI options" do
-  it "should run Generator.new.run" do
-    capture_io do
+describe "Hisyo::CLI" do
+  it "--help" do
+    out, err = capture_io do
       lambda { Hisyo::CLI.run(%w!--help!) }.should raise_error(SystemExit)
     end
+    out.split("\n").grep(/^Usage: /).should_not be_empty
   end
 end
